1c6a006c4deeb081f2701d4ed8ae1ab8347ed885
ha0goo <likethat0923@gmail.com>
2025-03-12 23:08:11 +0900

    첫커밋 다듬어지지않은

---
+++ app/login/page.js
@@ -0,0 +1,12 @@
+import LoginForm from './form';
+
+export default function LoginPage() {
+    return (
+        <div>
+            <h1>일단 로그인 페이지</h1>
+            <br/>
+            <LoginForm/>
+        </div>
+    );
+
+}
