862a4d2e4dc21fb928f7bd1f368978b80ad7d481
haegi <b_haegi@naver.com>
2025-03-14 16:20:47 +0900

    땡겨오기

--- app/games/page.js
+++ app/games/page.js
@@ -7,6 +7,7 @@ export default function page() {
       <div>
         <form>
           <input placeholder="제목" />
+          <input placeholder="카테고리" />
           <input placeholder="본문" />
           <button>생성</button>
         </form>
