dd88c61488f83362b6cf6e2fa0a068c9a8a68683
haegi <b_haegi@naver.com>
2025-03-14 15:08:17 +0900

    중간점검

--- app/games/page.js
+++ app/games/page.js
@@ -3,6 +3,14 @@ export default function page() {
     <div>
       <h1>게임 페이지</h1>
       <div>카테고리</div>
+
+      <div>
+        <form>
+          <input placeholder="제목" />
+          <input placeholder="본문" />
+          <button>생성</button>
+        </form>
+      </div>
     </div>
   );
 }
