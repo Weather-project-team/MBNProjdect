1e8ab130da091cd148bf0170b6d7790eab7250a3
ha0goo <likethat0923@gmail.com>
2025-03-24 16:03:24 +0900

    mypage route conflicted

--- app/api/auth/mypage/route.js
+++ app/api/auth/mypage/route.js
@@ -3,20 +3,6 @@ import { connectDB } from "@/lib/mongoose";
 import User from "@/models/User";
 
 export async function GET(req) {
-<<<<<<< HEAD
-  const session = await auth(); // auth()를 직접 호출
-  if (!session || !session.user) {
-    return Response.json({ error: "Unauthorized" }, { status: 401 });
-  }
-
-  await connectDB();
-  const user = await User.findOne({ email: session.user.email }).select(
-    "-password"
-  );
-
-  return Response.json(user);
-}
-=======
     const session = await auth();
     if (!session || !session.user) {
       return Response.json({ error: "Unauthorized" }, { status: 401 });
@@ -31,4 +17,3 @@ export async function GET(req) {
   
     return Response.json({ user }); // ✅ user를 key로 감싸줌
   }
->>>>>>> 8b84b95c7797ac1953f91997909f010fb4dc4b3f
